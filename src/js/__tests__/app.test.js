a4cfe98004174cee52c6a869cba49bb86b979688
Vladimim Grigoryev <grigoryevvladimir90@gmail.com>
2021-04-08 21:09:01 +0300

    fix

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,8 +1,10 @@
 import demo from '../app';
 
+// eslint-disable-next-line no-undef
 test('return', () => {
   const expected = 'VG';
   const result = demo('VG');
 
+  // eslint-disable-next-line no-undef
   expect(result).toBe(expected);
 });
